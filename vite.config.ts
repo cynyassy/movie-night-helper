8dc3ad8708c3c47b42bb99858a2e6ff0ebd451ac
cynyassy <45386951+cynyassy@users.noreply.github.com>
2025-08-05 13:57:38 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/movie-night-helper/', // <-- replace with your repo name
   plugins: [react()],
 })
